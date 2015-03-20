db03e3d82f29b6ba00a084085a26251fcb0915e0
Jan-Bo <jan.boettcher@gfk.com>
2015-03-20 14:21:32 +0100

    My answer to programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of these functions is to calculate the inverse of a matrix only once,
+## storing the result in a cache. In subsequent occasions, the inverse does not 
+## need to be computed again, but it will be retrieved directly from the cache.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The first function will create a list of functions to contain the matrix and its
+## inverse.
 
+makeCacheMatrix <- function(x = matrix()) {        
+        cachei <- NULL                                          ## i is the variable that will contain the cached
+                                                                ## inverse later. It is initialized to NULL.
+	
+        set <- function(y) {                                    ## set will store the input matrix (called y here)
+                x <<- y                                         ## in the function environment and set the cached inverse i to NULL
+                cachei <<- NULL                                 ## You could call this to initualize the object although
+        }                                                       ## calling just makeCacheMatrix does essentially the same (!).
+                                                                ## I suppose it is quicker than makeCacheMatrix, 
+                                                                ## so it may be useful if the object needs to be initialized
+                                                                ## multiple times in a loop...										
+
+        get <- function() x                                     ## delivers the matrix x stored inside the function
+
+        setinv <- function(replacei) {cachei <<- replacei}      ## setinv sets the cached inverse to a new value
+
+        getinv <- function() cachei                             ## getinv retrieves the cached inverse
+
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## The second function needs a "makeCacheMatrix" object as an argument. It will
+## first check whether the inverse has alrady been calculated. If yes, it just returns
+## the inverse that was previously stored. If not, it computes the inverse using
+## the 'solve' function.
+
+cacheSolve <- function(x) {
+        cachei <- x$getinv()                                    ## retrieve the cached inverse from the
+                                                                ## "makeCacheMatrix" object x
+
+        if(!is.null(cachei)) {                                  ## if the cache is not empty (i.e. not NULL)
+                message("getting cached data")                  ## and return the pre-calculated inverse 
+                return(cachei)
+        }
+	  else {                                                ## else (if the cache IS still empty)
+        	data <- x$get()	                                ## then get the data matrix stored inside the x object,
+        	i <- solve(data)                                ## compute its inverse
+        	x$setinv(i)                                     ## store it inside the x object	
+        	return(i)                                       ## and return it
+	  }
 }
+
+
